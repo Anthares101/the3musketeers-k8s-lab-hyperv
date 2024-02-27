91a944f2490f2fe88323742995b79206581f7bd7
Ángel Heredia <33127189+anthares101@users.noreply.github.com>
2024-02-27 19:54:02 +0100

    Update configure-static-ip.sh

--- scripts/configure-static-ip.sh
+++ scripts/configure-static-ip.sh
@@ -1,16 +1,21 @@
+# Be sure NOT to execute "systemctl restart network" here, so the changes take
+# effect on reboot instead of immediately, which would disconnect the provisioner.
+
 #!/bin/sh
 
 echo 'Setting static IP address for Hyper-V...'
 
-cat << EOF > /etc/sysconfig/network-scripts/ifcfg-eth0
-DEVICE=eth0
-BOOTPROTO=none
-ONBOOT=yes
-PREFIX=24
-IPADDR=$2
-GATEWAY=10.10.20.1
-DNS1=8.8.8.8
+cat << EOF > /etc/netplan/01-netcfg.yaml
+network:
+  version: 2
+  ethernets:
+    eth0:
+      dhcp4: no
+      addresses: [$2/24]
+      gateway4: 10.10.20.1
+      nameservers:
+        addresses: [8.8.8.8,8.8.4.4]
 EOF
 
-# Be sure NOT to execute "systemctl restart network" here, so the changes take
-# effect on reboot instead of immediately, which would disconnect the provisioner.
+# Be sure NOT to execute "netplan apply" here, so the changes take effect on
+# reboot instead of immediately, which would disconnect the provisioner.
