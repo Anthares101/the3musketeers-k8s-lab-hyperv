d157877076668cdc03850f4d0fa04f8e5cc51da6
Ángel Heredia <33127189+anthares101@users.noreply.github.com>
2024-04-29 01:34:41 +0200

    Update cleanup.ps1

--- scripts/cleanup.ps1
+++ scripts/cleanup.ps1
@@ -2,10 +2,10 @@ If((test-path -PathType container tmp)) {
     Remove-Item -LiteralPath "tmp" -Force -Recurse
 }
 
-If ("T3MSwitch" -in (Get-VMSwitch | Select-Object -ExpandProperty Name) -eq $FALSE) {
+If ("T3MSwitch" -in (Get-VMSwitch | Select-Object -ExpandProperty Name)) {
     Remove-VMSwitch -SwitchName "T3MSwitch" -Force
 }
 
-If ("10.10.20.0/24" -in (Get-NetNAT | Select-Object -ExpandProperty InternalIPInterfaceAddressPrefix) -eq $FALSE) {
+If ("10.10.20.0/24" -in (Get-NetNAT | Select-Object -ExpandProperty InternalIPInterfaceAddressPrefix)) {
     Remove-NetNat -Confirm:$false -Name "T3MNetwork"
 }
